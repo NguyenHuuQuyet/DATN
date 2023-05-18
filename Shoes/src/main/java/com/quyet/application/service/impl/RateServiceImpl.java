f27103e545d82b8bd35183b2279e0e2415493aea
NguyenHuuQuyet <102455597+NguyenHuuQuyet@users.noreply.github.com>
2023-05-18 20:30:07 +0700

    Update

--- Shoes/src/main/java/com/quyet/application/service/impl/RateServiceImpl.java
+++ Shoes/src/main/java/com/quyet/application/service/impl/RateServiceImpl.java
@@ -1,13 +1,13 @@
-//package com.vuhien.application.service.impl;
+//package com.quyet.application.service.impl;
 //
-//import com.vuhien.application.entity.Product;
-//import com.vuhien.application.entity.Rate;
-//import com.vuhien.application.entity.User;
-//import com.vuhien.application.exception.InternalServerException;
-//import com.vuhien.application.model.request.CreateRateProductRequest;
-//import com.vuhien.application.repository.ProductRepository;
-//import com.vuhien.application.repository.RateRepository;
-//import com.vuhien.application.service.RateService;
+//import com.quyet.application.entity.Product;
+//import com.quyet.application.entity.Rate;
+//import com.quyet.application.entity.User;
+//import com.quyet.application.exception.InternalServerException;
+//import com.quyet.application.model.request.CreateRateProductRequest;
+//import com.quyet.application.repository.ProductRepository;
+//import com.quyet.application.repository.RateRepository;
+//import com.quyet.application.service.RateService;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.stereotype.Component;
 //
